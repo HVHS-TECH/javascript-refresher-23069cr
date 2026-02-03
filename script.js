40937d827dfd8dc07dd1150e80d9b94cc61ea1fb
23069cr <23069cr@hvhs.school.nz>
2026-02-03 02:09:42 +0000

    prompts

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 alert("Congratulations!\nYou got the script running.")
+var response = prompt("Are you sure you wouold like to continue?\n Type yes or no")
+
+if(response == "yes"){
+    alert("go right ahead")
+}
+if(response == "no"){
+    alert("Too bad")
+} 
